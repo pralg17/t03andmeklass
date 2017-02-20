0ebb786b8f03811db1f798ae8198d1a1e2a351b8
kentloog <kentloog@greeny.cs.tlu.ee>
2017-02-20 18:58:28 +0200

    Kent Loog kodutöö

---
+++ proov.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class proov{
+	public static void main(String[] args) throws IOException{
+		uuring proov=new uuring("http://www.tlu.ee/~jaagup/andmed/muu/kobras2015.txt");
+		System.out.println("Suurim tulemus antud võistlusel oli " + proov.MaxTulemus(3) + " punkti");
+		System.out.println("Tallinna Reaalkoolist osales võistlusel " + proov.MituSakust(1) + " õpilast");
+
+	}
+}
