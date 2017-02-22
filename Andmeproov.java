8bf425ba6b2ea657b23e8125e30f990bd3722e5d
kirke gross <kirkegross@users.noreply.github.com>
2017-02-22 16:06:47 +0200

    Kodut88 kolm

--- Andmeproov.java
+++ Andmeproov.java
@@ -1,7 +1,9 @@
 import java.io.*;
 public class Andmeproov{
 	public static void main(String[] args) throws IOException{
-		Andmeuuring uuring=new Andmeuuring("http://www.tlu.ee/~jaagup/andmed/ilm/harkutund.txt");
-		System.out.println(uuring.maksimum(5));
+		Andmeuuring uuring=new Andmeuuring("http://www.tlu.ee/~jaagup/andmed/sots/synnid.csv");
+		System.out.println("2011. aastal oli väikseim sündide arv "+uuring.v2him(7)+".");
 	}
 }
+
+//2011. aastal oli väikseim sündide arv 98.
