551b5d218185f5b4f7bb4dbd140fccbc8ce36e09
richkaja <richkaja@greeny.cs.tlu.ee>
2017-02-22 17:47:15 +0200

    added console output

--- Main.java
+++ Main.java
@@ -7,3 +7,30 @@ public class Main{
 		
 	}
 }
+
+/* Console output
+
+[richkaja@greeny t03andmeklass]$ java Main 1
+Iron Maiden, 4195 fans
+
+[richkaja@greeny t03andmeklass]$ java Main 2
+�mheten, 0 fans
+
+[richkaja@greeny t03andmeklass]$ java Main 3
+--------------------------------
+| Top|Band                |Fans |
+--------------------------------
+|   1|Iron Maiden         |4195 |
+|   2|Iron Maiden         |4195 |
+|   3|Opeth               |4147 |
+|   4|Metallica           |3712 |
+|   5|Megadeth            |3105 |
+|   6|Amon Amarth         |3054 |
+|   7|Slayer              |2955 |
+|   8|Death               |2690 |
+|   9|Dream Theater       |2329 |
+|  10|Black Sabbath       |2307 |
+--------------------------------
+
+
+*/
