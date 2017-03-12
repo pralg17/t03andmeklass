17d8f65f1efd77d4815087d096f9e0d2ad4c751d
MK <mrxkollo@users.noreply.github.com>
2017-03-12 19:12:22 +0200

    Andmed sorteeritud

--- Data.java
+++ Data.java
@@ -1,5 +1,3 @@
-import com.sun.xml.internal.ws.util.StringUtils;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.Scanner;
@@ -12,14 +10,18 @@ public class Data{
 
 		String webUrl = "http://www.tlu.ee/~jaagup/andmed/muu/5klass.txt";
 		String out = new Scanner(new URL(webUrl).openStream(), "UTF-8").useDelimiter("\\A").next();
-		System.out.println(out);
-
-		String lines[] = out.split("\\r?\\n");
+		//System.out.println(out);
 
 
+		String[] lines = out.split("\n");
+		String[][] linesCsv = new String[lines.length][];
+
+		for( int i = 0; i < lines.length; i++ ){
+			linesCsv[i] = lines[i].split(",");
+		}
 
 
-		//IOUtils.toString(URL url)
+
 		//File text = new File("test.txt");
 		//text.createNewFile();
 
