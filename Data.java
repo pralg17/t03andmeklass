4b9e0e5d12cf3b79a60aefc7064abd407a156a01
MK <mrxkollo@users.noreply.github.com>
2017-03-13 21:02:50 +0200

    Struktureerimine vol 2

--- Data.java
+++ Data.java
@@ -4,26 +4,32 @@ import java.util.Scanner;
 
 public class Data{
 
+	String url;
+	String[][] data;
 
-	public static void main(String[] args) throws IOException {
+	public Data(String url) {
+		this.url = url;
+	}
+
+	public Data() throws IOException {
 
 
-		String webUrl = "http://www.tlu.ee/~jaagup/andmed/muu/5klass.txt";
-		String out = new Scanner(new URL(webUrl).openStream(), "UTF-8").useDelimiter("\\A").next();
-		//System.out.println(out);
-
+		String out = new Scanner(new URL(url).openStream(), "UTF-8").useDelimiter("\\A").next();
 
 		String[] lines = out.split("\n");
 		String[][] linesCsv = new String[lines.length][];
 
 		for( int i = 0; i < lines.length; i++ ){
 			linesCsv[i] = lines[i].split(",");
-		}
+			}
 
-
-
-		//File text = new File("test.txt");
-		//text.createNewFile();
-
+		this.data = linesCsv;
 	}
+
 }
+
+
+
+
+
+
---
+++ Run.java
@@ -0,0 +1,8 @@
+
+public class Run{
+
+	public static void main(String[] args) {
+		Data dt = new Data("http://www.tlu.ee/~jaagup/andmed/muu/5klass.txt");
+
+	}
+}
