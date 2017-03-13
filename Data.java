0102d6f51a941addb4d363a9aecde764cb286602
MK <mrxkollo@users.noreply.github.com>
2017-03-13 21:34:36 +0200

    End me pls. Constructoriga mängimine.

--- Data.java
+++ Data.java
@@ -4,27 +4,29 @@ import java.util.Scanner;
 
 public class Data{
 
-	String url;
 	String[][] data;
 
+
 	public Data(String url) {
-		this.url = url;
-	}
 
-	public Data() throws IOException {
+		String out = null;
 
-
-		String out = new Scanner(new URL(url).openStream(), "UTF-8").useDelimiter("\\A").next();
+		try{
+			out = new Scanner(new URL(url).openStream(), "UTF-8").useDelimiter("\\A").next();
+		}catch ( IOException e ){
+			e.printStackTrace();
+		}
 
 		String[] lines = out.split("\n");
 		String[][] linesCsv = new String[lines.length][];
 
 		for( int i = 0; i < lines.length; i++ ){
 			linesCsv[i] = lines[i].split(",");
-			}
+		}
 
 		this.data = linesCsv;
 	}
+
 
 }
 
--- Run.java
+++ Run.java
@@ -3,6 +3,7 @@ public class Run{
 
 	public static void main(String[] args) {
 		Data dt = new Data("http://www.tlu.ee/~jaagup/andmed/muu/5klass.txt");
+		System.out.println(dt.data[0][0]);
 
 	}
 }
