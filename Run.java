c081f2b86bc396f88ca1bcb580b74dbcb7e76ba5
MK <mrxkollo@users.noreply.github.com>
2017-03-14 07:25:44 +0200

    Faili salvestamine.

--- Run.java
+++ Run.java
@@ -1,12 +1,24 @@
+import java.io.IOException;
 
 public class Run{
 
-	public static void main(String[] args) {
-		Data dt = new Data("http://www.tlu.ee/~jaagup/andmed/muu/5klass.txt");
+public static void main(String[] args) throws IOException {
+	Data dt = new Data("http://www.tlu.ee/~jaagup/andmed/muu/5klass.txt");
+	String avgFem = "Naiste keskmine kogus on: " + Float.toString(dt.avgFemales()) + "\n";
+	String avgMales = "Meeste keskmine kogus on: " + Float.toString(dt.avgMales()) + "\n";
+	String avgHeight = "Inimeste keskmine pikkus on: " + Float.toString(dt.avgHeight()) + "\n";
+	String avgMass = "Inimeste keskmine kaal on: " + Float.toString(dt.avgMass()) + "\n";
 
-		//System.out.println(dt.data[0][0]);
-		System.out.println(dt.avgMales());
-		System.out.println(dt.avgFemales());
-		System.out.println(dt.avgHeight());
-	}
+
+	dt.writeData(avgFem, avgMales, avgHeight, avgMass);
+
+
+
+
+
+
+
+
+
+}
 }
