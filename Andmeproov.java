1575d84bfdb2f7786f0314d103e85788a34bbb2d
Rihardryys <Rihardryys@users.noreply.github.com>
2017-04-17 16:43:37 +0300

    Update Andmeproov.java

--- Andmeproov.java
+++ Andmeproov.java
@@ -2,6 +2,8 @@ import java.io.*;
 public class Andmeproov{
 	public static void main(String[] args) throws IOException{
 		Andmeuuring uuring=new Andmeuuring("http://www.tlu.ee/~jaagup/andmed/ilm/harkutund.txt");
-		System.out.println(uuring.maksimum(5));
+		System.out.println("Maksimum temperatuur on: "+uuring.maksimum(5));
+		System.out.println("Minimum temperatuur on: "+uuring.minimum(5));
+		System.out.println("Uuringute keskmine temp on: "+(uuring.maksimum(5))/(uuring.minimum(5)));
 	}
 }
