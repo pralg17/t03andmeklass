64e946c923699be5fc2c2ce6c40aee722940391c
hensav <hensav@gmail.com>
2017-04-27 15:02:07 +0300

    unustatud vana kodutöö, tulba numbri järgi on võimalik leida antud aastal suurim sündide arv kõikidest maakondadest

--- Andmeproov.java
+++ Andmeproov.java
@@ -1,7 +1,7 @@
 import java.io.*;
 public class Andmeproov{
-	public static void main(String[] args) throws IOException{
-		Andmeuuring uuring=new Andmeuuring("http://www.tlu.ee/~jaagup/andmed/ilm/harkutund.txt");
-		System.out.println(uuring.maksimum(5));
-	}
+    public static void main(String[] args) throws IOException{
+        Andmeuuring uuring=new Andmeuuring("http://www.tlu.ee/~jaagup/andmed/sots/synnid.csv");
+        System.out.println(uuring.maxSynnid(1));
+    }
 }
