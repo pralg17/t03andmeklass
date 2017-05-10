ef8d4dc70efa3ae04eca9f9d9070f081922d6f6b
Martin Holtsmeier <MartinHolts@users.noreply.github.com>
2017-05-10 13:30:27 +0300

    Add files via upload

--- Andmeproov.java
+++ Andmeproov.java
@@ -1,7 +1,7 @@
 import java.io.*;
 public class Andmeproov{
 	public static void main(String[] args) throws IOException{
-		Andmeuuring uuring=new Andmeuuring("http://www.tlu.ee/~jaagup/andmed/ilm/harkutund.txt");
-		System.out.println(uuring.maksimum(5));
+		Andmeuuring uuring=new Andmeuuring("http://www.tlu.ee/~jaagup/andmed/muu/kalevikommid.txt");
+		System.out.println(uuring.minimum(5));
 	}
 }
